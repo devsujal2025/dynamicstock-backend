8287b93b7fbaa65af103c12d940c89d8e0838e8c
devsujal2025 <sujalb2004@gmail.com>
2025-04-25 12:05:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -231,5 +231,7 @@ app.get("/api/search", async (req, res) => {
     }
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
